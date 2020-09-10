463d185c18dddb307f76e4bc2558e87928ab2b62
Sourav Das <iamsouravdaswade420@gmail.com>
2020-09-10 21:40:47 +0530

    selection sort in java

---
+++ selection_sort.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+class selection_sort
+{
+  public static void main(String[] args)
+   {
+     Scanner s=new Scanner(System.in);
+     int min,loc,i,j,n,temp;
+     System.out.println("Enter the elements length");
+     n=s.nextInt();
+     int[] a=new int[n];
+
+     System.out.println("Enter the elements");
+     for(i=0;i<n;i++)
+     {
+       a[i]=s.nextInt();
+     }
+     for (i=0;i<n-1 ;i++ )
+      {
+        min=a[i];
+        loc=i;
+        for (j=i+1;j<n;j++ )
+         {
+           if (a[j]<min)
+            {
+              min=a[j];
+              loc=j;
+
+           }
+
+
+        }
+        temp=a[i];
+        a[i]=a[loc];
+        a[loc]=temp;
+
+     }
+     System.out.println("The elements after selection sort");
+     for (i=0;i<n;i++ )
+      {
+        System.out.println(a[i]);
+
+     }
+
+  }
+}
