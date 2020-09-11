379df48273b0265f410f193271385665aede3ee1
Sourav Das <iamsouravdaswade420@gmail.com>
2020-09-11 10:57:52 +0530

    insertion sort practice in c++

---
+++ in.cpp
@@ -0,0 +1,31 @@
+#include "iostream"
+using namespace std;
+int main()
+{
+  int temp,i,j,n;
+  std::cout << "Enter the length" << '\n';
+  std::cin >> n;
+  int a[n];
+  std::cout << "Entr the elemnts" << '\n';
+  for(i=0;i<n;i++)
+  {
+    std::cin >> a[i];
+  }
+  for(i=1;i<n;i++)
+  {
+    temp=a[i];
+    j=i-1;
+    while (j>=0&&a[j]>temp)
+     {
+       a[j+1]=a[j];
+       j--;
+      /* code */
+    }
+    a[j+1]=temp;
+  }
+  std::cout << "The sorted list" << '\n';
+  for(i=0;i<n;i++)
+  {
+    std::cout << a[i] << " ";
+  }
+}
